fa2f6a7887238cefa5548091b53487081ec7be17
Nguyễn Quang Thắng <thangnq96tb@gmail.com>
2024-05-25 08:43:21 +0700

    *random change direction of monster when moving at specific position (defined path then random pick)

--- Assets/Script/MonsterController.cs
+++ Assets/Script/MonsterController.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     [SerializeField] private float m_Speed = 100f;
-    [SerializeField] private List<Transform> path = new List<Transform>();
+    [SerializeField] private List<Transform> path_1 = new List<Transform>();
+    [SerializeField] private List<Transform> path_2 = new List<Transform>();
 
     private int point_Index = 0;
+    private List<Transform> monsterPath = new List<Transform>();
 
     void Start()
     {
-        
+        if(Random.Range(0,2) == 0)
+        {
+            Debug.Log("Monster will moving on the left");
+            monsterPath = path_1;
+        }
+        else
+        {
+            Debug.Log("Monster will moving on the right");
+            monsterPath = path_2;
+        }
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, path[point_Index].position, 0.01f);
-        if(Vector2.Distance(transform.position, path[point_Index].position) < 0.1f && point_Index < path.Count - 1)
+        transform.position = Vector2.MoveTowards(transform.position, monsterPath[point_Index].position, 0.01f);
+        if(Vector2.Distance(transform.position, monsterPath[point_Index].position) < 0.1f && point_Index < monsterPath.Count - 1)
         { 
             point_Index++;
         }
