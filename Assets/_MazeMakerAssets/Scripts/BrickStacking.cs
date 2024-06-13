22b1c00d544a317711cdec90cdf7c8e7d16c3992
Nguyen Van Trung <149683808+trungq108@users.noreply.github.com>
2024-06-13 23:02:42 +0700

    13-6 Unbrick and CameraFollow

--- Assets/_MazeMakerAssets/Scripts/BrickStacking.cs
+++ Assets/_MazeMakerAssets/Scripts/BrickStacking.cs
@@ -16,7 +16,13 @@ public class BrickStacking : MonoBehaviour
         player = GetComponent<Player>();
     }
 
-    public void AddBrick()
+    public void StackChecking()
+    {
+        AddBrick();
+        RemoveBrick();
+    }
+
+    private void AddBrick()
     {
         if(Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit ,5f, 1 << 8))
         {
@@ -29,8 +35,21 @@ public class BrickStacking : MonoBehaviour
         }
     }
 
-    public void RemoveBrick()
+    private void RemoveBrick()
     {
-
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 5f, 1 << 9))
+        {
+            if(brickStack.Count > 0)
+            {
+                Transform removedBrick = brickStack.Pop();
+                removedBrick.SetParent(null);
+                removedBrick.transform.position = hit.point;
+                playerModel.transform.localPosition -= new Vector3(0, brickWight, 0);
+            }
+            else
+            {
+                Debug.Log("Game Over");
+            }
+        }
     }
 }
--- Assets/_MazeMakerAssets/Scripts/Player.cs
+++ Assets/_MazeMakerAssets/Scripts/Player.cs
@@ -10,11 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] Transform dowm;
 
     private BrickStacking brickStacking;
-
-    public bool canMoveUp;
-    public bool canMoveDown;
-    public bool canMoveLeft;
-    public bool canMoveRight;
+    public bool canMove = true;
 
     private void Awake()
     {
@@ -23,35 +19,41 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (!canMove) return;
+
         if (Input.GetKeyDown(KeyCode.A))
         {
-            StartCoroutine(Move(canMoveLeft, left, -Vector3.right));
+            StartCoroutine(Move(left, -Vector3.right));
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            StartCoroutine(Move(canMoveRight, right, Vector3.right));
+            StartCoroutine(Move(right, Vector3.right));
         }
         else if(Input.GetKeyDown(KeyCode.W))
         {
-            StartCoroutine(Move(canMoveUp, up, Vector3.forward));
+            StartCoroutine(Move(up, Vector3.forward));
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            StartCoroutine(Move(canMoveDown, dowm, -Vector3.forward));
+            StartCoroutine(Move(dowm, -Vector3.forward));
         }
     }
 
-    IEnumerator Move(bool canMove, Transform directionPoint, Vector3 moveDirection)
+    IEnumerator Move(Transform directionPoint, Vector3 moveDirection)
     {
         yield return new WaitForSeconds(0.05f);
 
-        canMove = Physics.Raycast(directionPoint.position, Vector3.down, 5f, 1 << 7);
-        if (canMove)
+        bool canMoveThisDirect = Physics.Raycast(directionPoint.position, Vector3.down, 5f, 1 << 7);
+        if (canMoveThisDirect)
         {
+            canMove = false;
             transform.Translate(moveDirection);
-            brickStacking.AddBrick();
-            StartCoroutine(Move(canMove, directionPoint, moveDirection));
+            brickStacking.StackChecking();
+            StartCoroutine(Move(directionPoint, moveDirection));
         }
-        else StopAllCoroutines();
+        else
+        {
+            canMove = true;
+        }
     }
 }
