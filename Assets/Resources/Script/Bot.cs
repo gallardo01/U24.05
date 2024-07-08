3090b9463ab812b3d7465332eed999effefa0871
Nguyễn Quang Thắng <thangnq96tb@gmail.com>
2024-07-08 19:20:29 +0700

    *moving bot using nav mesh

--- Assets/Resources/Script/Bot.cs
+++ Assets/Resources/Script/Bot.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Bot : Character
 {
     IState<Bot> currentState;
+    public NavMeshAgent agent;
+    public bool isDestination => Vector3.Distance(destination, Vector3.right * transform.position.x + Vector3.forward * transform.position.z) < 0.1f; //dynamic check 
+    private Vector3 destination;
 
     private void Start()
     {
         ChangeState(new IdleState());
     }
+
+    public void SetDestination(Vector3 position)
+    {
+        agent.enabled = true;
+        destination = position;
+        destination.y = 0;
+        agent.SetDestination(position);
+    }    
 
     private void Update()
     {
--- Assets/Resources/Script/State/RunningState.cs
+++ Assets/Resources/Script/State/RunningState.cs
@@ -7,34 +7,46 @@ public class RunningState : IState<Bot>
     Vector3 target; 
     public void OnEnter(Bot bot)
     {
-        //bot.target = new Vector3(Random.Range(-9f, 9f), 0.35f, Random.Range(-9f, 9f));
-        if(bot.stage.GetNearestBricks(bot) != null)
-        {
-            target = bot.stage.GetNearestBricks(bot).position;
-            target.y = bot.transform.position.y;
-        } 
-        else
-        {
-            bot.ChangeState(new IdleState());
-        }
+        bot.ChangeAnim("run");
+        SeekTarget(bot);
     }
 
     public void OnExecute(Bot bot)
     {
-        bot.transform.position = Vector3.MoveTowards(bot.transform.position, target, 0.03f);
+        //bot.transform.position = Vector3.MoveTowards(bot.transform.position, target, 0.03f);
 
-        Vector3 direction = target - bot.transform.position;// + offset;
-        Quaternion targetRotation = Quaternion.LookRotation(direction);
-        bot.transform.rotation = Quaternion.Slerp(bot.transform.rotation, targetRotation, 0.125f);
+        //Vector3 direction = target - bot.transform.position;// + offset;
+        //Quaternion targetRotation = Quaternion.LookRotation(direction);
+        //bot.transform.rotation = Quaternion.Slerp(bot.transform.rotation, targetRotation, 0.125f);
 
-        if((target - bot.transform.position).magnitude < 0.1f)
+        //if((target - bot.transform.position).magnitude < 0.1f)
+        //{
+        //    bot.ChangeState(new IdleState());
+        //}
+
+        if(bot.isDestination)
         {
+            //SeekTarget(bot);
             bot.ChangeState(new IdleState());
         }
     }
 
     public void OnExit(Bot bot)
     {
+        bot.ChangeAnim("idle");
+    }
 
+    public void SeekTarget(Bot bot)
+    {
+        if (bot.stage.GetNearestBricks(bot) != null)
+        {
+            target = bot.stage.GetNearestBricks(bot).position;
+            target.y = bot.transform.position.y;
+        }
+        else
+        {
+            bot.ChangeState(new IdleState());
+        }
+        bot.SetDestination(target);
     }
 }
