877e854c4974ed1e774c1be4c8c985023e4bb221
Sonbin8193 <126962131+Sonbin8193@users.noreply.github.com>
2024-08-11 21:09:50 +0700

    add data json

--- Assets/_Script/Common/ItemJSONDatabase.cs
+++ Assets/_Script/Common/ItemJSONDatabase.cs
@@ -3,19 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
 using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
 
 public class ItemJSONDatabase : MonoBehaviour
 {
     private JsonData itemData;
+    private JsonData inGameItemData;
     private List<Item> listItem = new List<Item>();
+    private List<GameItem> listInGameItem = new List<GameItem>();
+    private string filePath = "MyItem.txt";
     // Start is called before the first frame update
     void Start()
     {
         LoadResourceFromTxt();
         ConstructDatabase();
-        Debug.Log(listItem[0].iD);
+        LoadDataFromLocalDb();
     }
 
+    private void LoadDataFromLocalDb()
+    {
+        string filePathFull = Application.persistentDataPath + "/" + filePath;
+        if (File.Exists(filePathFull))
+        {
+            byte[] jsonByte = null;
+            try
+            {
+                jsonByte = File.ReadAllBytes(filePathFull);
+            } catch
+            {
+
+            }
+            string jsonData = Encoding.ASCII.GetString(jsonByte);
+            inGameItemData = JsonMapper.ToObject(jsonData);
+            ConstructMyItemDatabase();
+            //TextAsset targetFile = Resources.Load<TextAsset>(filePath);
+            //itemData = JsonMapper.ToObject(targetFile.text);
+        }
+        else
+        {
+            AddNewItemFirstTime();
+            SaveDataToLocalDb();
+        }
+    }
+
+    private void AddNewItemFirstTime()
+    {
+        for (int i = 0; i < listItem.Count; i++)
+        {
+            GameItem gameItem = new GameItem();
+            gameItem.item = listItem[i];
+            gameItem.Purchase = false;
+            gameItem.Equip = false;
+            listInGameItem.Add(gameItem);
+        }
+    }
+
+    private void SaveDataToLocalDb()
+    {
+        string JsonData = JsonConvert.SerializeObject(listInGameItem.ToArray(),Formatting.Indented);
+        string filePathFull = Application.persistentDataPath + "/" + filePath;
+        byte[] jsonByte = Encoding.ASCII.GetBytes(JsonData);
+        if (!Directory.Exists(Path.GetDirectoryName(filePathFull)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(filePathFull));
+        }
+        if (!File.Exists(filePathFull))
+        {
+            File.Create(filePathFull).Close(); 
+        }
+        try
+        {
+            File.WriteAllBytes(filePathFull, jsonByte);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
     private void LoadResourceFromTxt()
     {
         string filepath = "StreamingAsset/Item";
@@ -23,30 +89,59 @@ public class ItemJSONDatabase : MonoBehaviour
         itemData = JsonMapper.ToObject(targetFile.text);
 
     }
+    private void ConstructMyItemDatabase()
+    {
+        for (int i = 0; i < inGameItemData.Count; i++)
+        {
+            listInGameItem.Add(new GameItem()
+            {
+                Purchase = (bool)inGameItemData[i]["Purchase"],
+                Equip = (bool)inGameItemData[i]["Equip"],
+                item = new Item()
+                {
+                    id = (int)inGameItemData[i]["item"]["id"],
+                    type = (string)inGameItemData[i]["item"]["type"],
+                    name = (string)inGameItemData[i]["item"]["name"],
+                    price = (int)inGameItemData[i]["item"]["price"],
+                    atk = (int)inGameItemData[i]["item"]["atk"],
+                    def = (int)inGameItemData[i]["item"]["def"],
+                    speed = (int)inGameItemData[i]["item"]["speed"]
+                }               
+            });
 
+        }
+    }
     private void ConstructDatabase()
     {
         for (int i = 0; i < itemData.Count; i++)
         {
             listItem.Add(new Item()
             {
-                iD = (int)itemData[i]["Id"],
-                type = (string)itemData[i]["Type"],
-                price = (int)itemData[i]["Price"],
-                atk = (int)itemData[i]["Atk"],
-                def = (int)itemData[i]["Def"],
-                speed = (float)itemData[i]["Spd"]
+                id = (int)itemData[i]["id"],
+                type = (string)itemData[i]["type"],
+                name = (string)itemData[i]["name"],
+                price = (int)itemData[i]["price"],
+                atk = (int)itemData[i]["atk"],
+                def = (int)itemData[i]["def"],
+                speed = (int)itemData[i]["speed"]
             });
         }
     }
 }
 
+public class GameItem
+{
+    public bool Purchase { get; set; }
+    public bool Equip { get; set; }
+    public Item item { get; set; }
+}
 public class Item
 {
-    public int iD { get; set; }
+    public int id { get; set; }
     public string type { get; set; }
+    public string name { get; set; }
     public int price { get; set; }
     public int atk { get; set; }
     public int def { get; set; }
-    public float speed { get; set; }
+    public int speed { get; set; }
 }
---
+++ Assets/_Script/UI/InitItem.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static UnityEditor.Progress;
+public class InitItem : MonoBehaviour
+{
+    public Image itemImage;
+    public GameObject lockItem;
+    public GameObject buyButton;
+    public GameObject equipButton;
+    public TMPro.TextMeshProUGUI priceText;
+    public TMPro.TextMeshProUGUI equipItem;
+
+    public enum PurchaseState
+    {
+        EQUIPED,
+        EQUIP,
+        PURCHASE
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InitItemUI(GameItem item, PurchaseState state)
+    {
+        itemImage.sprite = Resources.Load<Sprite>("UI/Hat" + item.item.name);
+        if (item.Purchase)
+        {
+            BuyStateUI(PurchaseState.PURCHASE);
+            priceText.text = item.item.price.ToString();
+        }
+        else
+        {
+            if (item.Equip)
+            {
+               BuyStateUI(PurchaseState.EQUIPED);
+               equipButton.GetComponent<Button>().enabled = false;
+            }
+            else
+            {
+                BuyStateUI(PurchaseState.EQUIP);
+                equipButton.GetComponent<Button>().enabled = true;
+            }
+        }
+    }
+
+    public void BuyStateUI(PurchaseState state)
+    {
+        switch (state)
+        {
+            case PurchaseState.EQUIPED:
+                lockItem.SetActive(false);
+                buyButton.SetActive(false);
+                equipItem.text = "Equiped";
+                equipButton.SetActive(true);
+                break;
+            case PurchaseState.EQUIP:
+                lockItem.SetActive(false);
+                buyButton.SetActive(false);
+                equipItem.text = "Equip";
+                equipButton.SetActive(true);
+                break;
+            case PurchaseState.PURCHASE:
+                lockItem.SetActive(true);
+                buyButton.SetActive(true);
+                equipButton.SetActive(false);
+                break;
+        }
+    }
+}
